48f33a9a768092ce0ba5e5dfb745a3303f52e042
TadoHopsky <frolov.dev1375@gmail.com>
2024-11-17 14:36:05 +0700

    Добавлено сравнение по времени на добавление в Базу Данных с помощью Batch Update и стандартного добавления + добавлено улучшение визуализации главной страницы приложения

--- src/main/java/webapp/app/crudsringbootmain/DAO/PersonDao.java
+++ src/main/java/webapp/app/crudsringbootmain/DAO/PersonDao.java
@@ -1,11 +1,15 @@
 package webapp.app.crudsringbootmain.DAO;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import webapp.app.crudsringbootmain.user.Person;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -19,30 +23,86 @@ public class PersonDao {
     }
 
     // Show all users in DB
-    public List<Person> index(){
+    public List<Person> index() {
         return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     // Show one user from DB found by ID
-    public Person show(int id){
+    public Person show(int id) {
         return jdbcTemplate.query("SELECT * FROM Person where id=?", new Object[]{id},
                 new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
     }
 
     // Add new user from DB
-    public void create(Person person){
+    public void create(Person person) {
         jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)",
                 person.getName(), person.getEmail(), person.getLink());
     }
 
     // Update User information
-    public void update(int id, Person updatePerson){
+    public void update(int id, Person updatePerson) {
         jdbcTemplate.update("UPDATE Person SET name=?, email=?, link=? WHERE id=?",
                 updatePerson.getName(), updatePerson.getEmail(), updatePerson.getLink(), id);
     }
 
     // Delete user from DB
-    public void delete(int id){
+    public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person where id=?", id);
+    }
+
+    /** ================================================================================================ **/
+    /** ==================================== Batch Update Test ========================================= **/
+    /** ================================================================================================ **/
+
+
+    public void testMultiplyUpdate() {                                       //  Time : 213
+        List<Person> people = create1000Users();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person : people) {
+            jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?)",
+                    person.getId(), person.getName(), person.getEmail(), person.getLink());
+        }
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time : " + (after - before));
+    }
+
+    public void testBatchUpdate() {                                          //  Time : 44
+        List<Person> people = create1000Users();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES(?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setInt(1, people.get(i).getId());
+                        ps.setString(2, people.get(i).getName());
+                        ps.setString(3, people.get(i).getEmail());
+                        ps.setString(4, people.get(i).getLink());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time : " + (after - before));
+    }
+
+    // Создание списка из 1000 людей
+    private List<Person> create1000Users() {
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "name" + i, "name" + i + "@mail.ru", "@name" + i));
+        }
+        return people;
     }
 }
---
+++ src/main/java/webapp/app/crudsringbootmain/controller/BatchMethodController.java
@@ -0,0 +1,37 @@
+package webapp.app.crudsringbootmain.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import webapp.app.crudsringbootmain.DAO.PersonDao;
+
+@Controller
+@RequestMapping("/batch-page")
+public class BatchMethodController {
+
+    private PersonDao personDao;
+
+    @Autowired
+    public BatchMethodController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping()
+    public String index(){
+        return "/batch/index";
+    }
+
+    @GetMapping("/without")
+    public String updateWithout(){
+        personDao.testMultiplyUpdate();
+        return "redirect:/people";
+    }
+
+    @GetMapping("/with")
+    public String updateWith(){
+        personDao.testBatchUpdate();
+        return "redirect:/people";
+    }
+}
